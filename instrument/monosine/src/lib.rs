dc8a1be025f5d5f1cfc25f6fd52d7be58f54975e
John Else <john.else@gmail.com>
2018-01-29 00:01:43 +0000

    monosine: respond to MIDI note on/off events

--- instrument/monosine/src/lib.rs
+++ instrument/monosine/src/lib.rs
@@ -2,17 +2,48 @@
 
 #[macro_use] extern crate vst;
 
+use vst::api::{Events, Supported};
 use vst::buffer::AudioBuffer;
-use vst::plugin::{Category, Info, Plugin};
+use vst::event::Event;
+use vst::plugin::{Category, CanDo, Info, Plugin};
 
 struct MonoSine {
     frequency: f32,
     level: f32,
     theta: f32,
     sample_rate: f32,
+    note: Option<u8>,
 }
 
 pub const TAU: f32 = std::f32::consts::PI * 2.0;
+
+fn midi_pitch_to_freq(pitch: u8) -> f32 {
+    const A4_PITCH: i8 = 69;
+    const A4_FREQ: f32 = 440.0;
+
+    // Midi notes can be 0-127
+    ((f32::from(pitch as i8 - A4_PITCH)) / 12.).exp2() * A4_FREQ
+}
+
+impl MonoSine {
+    fn process_midi_event(&mut self, data: [u8; 3]) {
+        match data[0] {
+            128 => self.note_off(data[1]),
+            144 => self.note_on(data[1]),
+            _ => (),
+        }
+    }
+
+    fn note_on(&mut self, note: u8) {
+        self.note = Some(note);
+    }
+
+    fn note_off(&mut self, note: u8) {
+        if self.note == Some(note) {
+            self.note = None;
+        }
+    }
+}
 
 impl Default for MonoSine {
     fn default() -> MonoSine {
@@ -21,6 +52,7 @@ impl Default for MonoSine {
             level: 1.0,
             theta: 0.0,
             sample_rate: 44100.0,
+            note: None,
         }
     }
 }
@@ -40,6 +72,13 @@ impl Plugin for MonoSine {
 
             // fill in the rest with the default values
             ..Info::default()
+        }
+    }
+
+    fn can_do(&self, can_do: CanDo) -> Supported {
+        match can_do {
+            CanDo::ReceiveMidiEvent => Supported::Yes,
+            _ => Supported::Maybe,
         }
     }
 
@@ -81,20 +120,36 @@ impl Plugin for MonoSine {
     }
 
     fn process(&mut self, buffer: &mut AudioBuffer<f32>) {
-        let samples = buffer.samples();
-        let (_, outputs) = buffer.split();
+        match self.note {
+            Some(note) => {
+                self.frequency = midi_pitch_to_freq(note);
 
-        for output_buffer in outputs {
-            let mut theta = self.theta;
+                let samples = buffer.samples();
+                let (_, outputs) = buffer.split();
 
-            for output_sample in output_buffer {
+                for output_buffer in outputs {
+                    let mut theta = self.theta;
 
-                *output_sample = theta.sin() * self.level;
-                theta += TAU * self.frequency / self.sample_rate;
+                    for output_sample in output_buffer {
+
+                        *output_sample = theta.sin() * self.level;
+                        theta += TAU * self.frequency / self.sample_rate;
+                    }
+                }
+
+                self.theta += samples as f32 *  TAU * self.frequency / self.sample_rate;
+            }
+            None => (),
+        }
+    }
+
+    fn process_events(&mut self, events: &Events) {
+        for event in events.events() {
+            match event {
+                Event::Midi(ev) => self.process_midi_event(ev.data),
+                _ => (),
             }
         }
-
-        self.theta += samples as f32 *  TAU * self.frequency / self.sample_rate;
     }
 }
 
